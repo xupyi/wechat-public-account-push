9a0db972e40059c8708d34ae081f01e93cb82884
xpy <43030712+xupyi@users.noreply.github.com>
2022-10-11 11:24:09 +0800

    3

--- config/index.cjs
+++ config/index.cjs
@@ -5,62 +5,44 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wxa093d71ec9c445eb',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '0dfd464b3bc6df76469b62163818d9b6',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ollwI6lvhiaKp6-Yifu8jsXw3SNs',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'LUTXFrKdGZfycODHki0OUMkHcn6C56h-DoqL4st6V9Q',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'LUTXFrKdGZfycODHki0OUMkHcn6C56h-DoqL4st6V9Q',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ollwI6lvhiaKp6-Yifu8jsXw3SNs',
-    }
-  ],
-
+const USER_CONFIG ={
+	"APP_ID": "wxa093d71ec9c445eb",
+	"APP_SECRET": "0dfd464b3bc6df76469b62163818d9b6",
+	"IS_SHOW_COLOR": true,
+	"CALLBACK_TEMPLATE_ID": "",
+	"CALLBACK_USERS": [{
+		"name": "自己",
+		"id": ""
+	}],
+	"USERS": [{
+		"name": "想",
+		"id": "wxa093d71ec9c445eb",
+		"useTemplateId": "0dfd464b3bc6df76469b62163818d9b6",
+		"province": "浙江",
+		"city": "杭州",
+		"horoscopeDate": "10-11",
+		"horoscopeDateType": "今日",
+		"openUrl": "https://shuangxunian.github.io/",
+		"festivals": [{
+			"type": "*生日",
+			"name": "嗯嗯",
+			"date": "10-12",
+			"year": "2022"
+		}],
+		"customizedDateList": []
+	}],
+	"SWITCH": {
+		"weather": true,
+		"holidaytts": true,
+		"CIBA": true,
+		"oneTalk": false,
+		"earthyLoveWords": false,
+		"momentCopyrighting": false,
+		"poisonChickenSoup": false,
+		"poetry": false,
+		"horoscope": false,
+		"birthdayMessage": true
+	}
 }
 
 module.exports = USER_CONFIG
