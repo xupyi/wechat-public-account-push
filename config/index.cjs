a49c3796dcbf820225ec2054baa41e1ebcbc8fb8
xpy <43030712+xupyi@users.noreply.github.com>
2022-10-20 14:21:30 +0800

    Update index.cjs
     {{birthday_message.DATA}} {{moment_copyrighting.DATA}} {{poetry_title.DATA}} {{poetry_content.DATA}}

--- config/index.cjs
+++ config/index.cjs
@@ -35,30 +35,46 @@ const USER_CONFIG ={
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ollwI6lvhiaKp6-Yifu8jsXw3SNs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '9hJ_1VMWxpqL1Pz8Jg_1ciiNoWDjVWdmVwi6bMgeZm0',
+      useTemplateId: 'QlCWuAKo9HIiM2OCKiIZZ-1IbKFKxQb_cL7fVynWO_k',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-10',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '小乔宝贝', year: '1997', date: '01-04',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '*生日', name: '刘茵宝贝', year: '1998', date: '03-20',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+        
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2012-09-09' },
+        { keyword: 'love_day', date: '2013-09-01' },
       ],
     },
+  ],
+	/** 插槽 */
+
+  /** 你可以在这里写超多的你想显示的内容了！
+   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
+   * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
+   * */
+  SLOT_LIST: [
+    // 这样配置的话，就会每次随机选一句话发送
+    {
+      keyword: 'lover_prattle',
+      contents: [
+        '因为太喜欢你，所以看谁都像是情敌。',
+        '申请成为你爱里的永久居民。',
+        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
+        '遇见你，就好像捡到了100斤的运气'
+      ],
+    }
+    // 你可以不断按格式往下增加
+    // ...
   ],
 }
 
