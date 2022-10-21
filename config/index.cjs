d12aecb8c0ea12227960cf433b7b5e41407210b4
xpy <43030712+xupyi@users.noreply.github.com>
2022-10-21 09:25:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -83,7 +83,7 @@ const USER_CONFIG ={
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ollwI6ilJKtKomzUMocsTLm3-v-A',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'hYG0Qt6goocKMeoArB1ah3hruSQ7rWFenX5gZ8gBh5E',
+      useTemplateId: 'gd3ddzJ5yxIZThL_pPXEHLc5azodKCjUHetqpxWRef4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-10',
       festivals: [
